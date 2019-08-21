53cda3233fc9e4119061ab2c32fe5a51557d2de8
ssephillip <phillip.sittigweb.de>
2019-08-21 15:19:32 +0200

    Uses newer image; Starts front end in seperate container

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+#Prerequisites:
+#1. git
+#2. docker
+#3. docker-compose
 git clone https://github.com/ssephillip/newsleak-docker.git
 cd newsleak-docker
 docker-compose up -d
 docker exec -it -u root newsleak ./newsleak-start.sh
+docker-compose -f docker-compose.front-end.yml up -d
 echo "Setup finished. Application started."
