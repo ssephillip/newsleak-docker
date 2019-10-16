45e801aec3b3e5e0bcdf068b2aba262ec8f2bebf
ssephillip <phillip.sittigweb.de>
2019-10-16 21:04:08 +0200

    bugfix install script

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ git clone https://github.com/ssephillip/newsleak-docker.git
 cd newsleak-docker
 docker network create hoover_default
 docker-compose up -d
-docker exec -it -u root newsleak ./newsleak-start.sh
+docker exec -it -u root newsleak ./newsleak-init.sh
 docker-compose stop
 echo "Setup finished. You can now start the application."
