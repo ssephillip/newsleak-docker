46cbe57ce653ff4c9f2f707c136cecf0cd032366
ssephillip <phillip.sittigweb.de>
2020-03-30 16:18:46 +0200

    adds install script to newsleak-docker repository and makes the necessary changes

---
+++ install-newsleak.sh
@@ -0,0 +1,25 @@
+#Sets up the folders needed to run the docker-compose setup.
+
+#Prerequisites:
+#1. docker
+#2. docker-compose
+#-----------------------------------------------------------
+
+mkdir -p volumes/ui/conf/dictionaries
+mkdir -p volumes/ui/data
+mkdir -p volumes/ui/doc2vec-training/result
+chmod 777 -R volumes/ui
+docker network create hoover_default
+docker-compose up -d
+docker exec -it -u root newsleak ./newsleak-start.sh
+docker-compose stop
+
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
+echo "Setup finished."
+echo "You can now start the application."
+echo "To start enter: docker-compose up -d"
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
